e4391d93facb9aacd4543eb1eb2d11a439463ae9
Kandace Willcox <kiwdev1984@icloud.com>
2019-08-15 16:06:10 +1200

    setup basic file structure

---
+++ routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const data = require('./data.json')
+const fs = require('fs')
+module.exports = router
+
+router.get('/', (req, res) => {
+  console.log("Hello mealPoll Team!!")
+
+})
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const routes = require('./routes')
+const puppies = require('./data.json')
+
+const server = express()
+
+const port = process.env.PORT || 3000
+
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+server.use('/', routes)
+
+
+
+module.exports = server
